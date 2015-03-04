35e2d45a49aea7563477d2ffff6a778831154e47
Engin Kurutepe <engin@kurutepe.com>
2015-03-04 12:46:13 +0100

    show workunits on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,9 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-
+    if current_user 
+      @workunits = current_user.workunits
+    end
   end
 
 end
