04342117269caf8f3b23c29df29e8242a6e7514c
Engin Kurutepe <engin@kurutepe.com>
2015-03-07 22:16:46 +0100

    display workunits

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,8 +20,8 @@ class ProjectsController < ApplicationController
     admin_ids = params[:admin_ids]
     unless admin_ids.nil?
       admins = User.find_all_by_id(admin_id)
-      @project.admins = admins
-      @project.save
+      project.admins = admins
+      project.save
     end
     redirect_to projects_path
   end
