e7ae61f4d40ce15804f8a11a4f7af8d590679ea4
Engin Kurutepe <engin@kurutepe.com>
2015-03-12 20:56:48 +0100

    use comma as currency seperator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def number_to_euro(amount)
-    number_to_currency(amount,:unit=>'€', format: "%n %u")
+    number_to_currency(amount,:unit=>'€', separator: ",", format: "%n %u")
   end
   
 end
